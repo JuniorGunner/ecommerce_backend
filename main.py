188009157fc465cea6b99d533be80394a9d8bc88
JuniorGunner <csf.junior90@gmail.com>
2024-01-15 21:46:07 -0300

    core: main file initial setup

---
+++ main.py
@@ -0,0 +1,18 @@
+# main.py
+from fastapi import FastAPI
+from routes import (
+    user_routes,
+    product_routes,
+    order_routes,
+    cart_routes,
+    analytics_routes
+)
+
+app = FastAPI()
+
+# Including different route modules
+app.include_router(user_routes.router)
+app.include_router(product_routes.router)
+app.include_router(order_routes.router)
+app.include_router(cart_routes.router)
+app.include_router(analytics_routes.router)
